3876f02093b3a84ed305a047c51e29b3a7e5984e
amitjoshi9627 <amitjoshi9627@gmail.com>
2020-01-19 23:31:12 +0530

    Worked on Issue #8

--- ubuntu_requirement_fix.py
+++ ubuntu_requirement_fix.py
@@ -1,4 +1,11 @@
 import os
+import platform
 
-def ubuntu_req_fix():
-    os.system('sudo apt install ffmpeg')
+def ubuntu_req_fix(): 
+    os_version = platform.version()
+    if '18.' in os_version:
+        os.system('sudo apt install ffmpeg')
+    else:
+        os.system('sudo add-apt-repository ppa:jonathonf/ffmpeg-4')
+        os.system('sudo apt update')
+        os.system('sudo apt remove ffmpeg && sudo apt install -y ffmpeg')
