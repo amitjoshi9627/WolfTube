28a66d774cc5d45e6e0fe4ced5b079744dd6b9df
amitjoshi9627 <amitjoshi9627@gmail.com>
2020-08-25 08:15:04 +0530

    Added Streamlit Web app

---
+++ src/app.py
@@ -0,0 +1,44 @@
+import streamlit as st
+import time
+import wolftube
+
+st.title('WolfTube')
+
+url = st.text_input('Enter the URL of the YouTube video')
+
+download_type = st.radio("",("Audio","Video","Audio Playlist","Video Playlist"))
+
+if st.button("Download"):
+    if url:
+        try:
+            if download_type == "Audio":
+                audio_downloaded,if_error = wolftube.Download_file((2,url)) 
+                if audio_downloaded:
+                    st.success("File Downloaded to Audio/ ")
+                else:
+                    st.error(if_error)
+
+            elif download_type == "Video":
+                video_downloaded,if_error = wolftube.Download_file((1,url))
+                if video_downloaded:
+                    st.success("File Downloaded to Video/ ")
+                else:
+                    st.error(if_error)
+
+            elif download_type == "Video Playlist":
+                audio_downloaded,if_error = wolftube.Download_file((3,url))
+                if audio_downloaded:
+                    st.success("Files Downloaded to Audio/ ")
+                else:
+                    st.error(if_error)
+
+            else:
+                video_downloaded,if_error = wolftube.Download_file((4,url))
+                if video_downloaded:
+                    st.success("Files Downloaded to Video/ ")
+                else:
+                    st.error(if_error)
+
+        except Exception as e:
+            print(e)
+            st.error("Error occured! Please check the URL, Refresh the Page and Try Again!")
